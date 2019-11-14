eb1386a7cf1d6dbb70327aee6dc2def1647325fd
manojdvs7 <52916630+manojdvs7@users.noreply.github.com>
2019-11-14 20:17:21 +0530

    Update commonWords.cpp

--- C-Strings/src/commonWords.cpp
+++ C-Strings/src/commonWords.cpp
@@ -29,29 +29,34 @@ char* getWord(char* str,int *pos)
 	return word;
 }
 
+void compareWordsWithString2(char **result, char* str2, int pos2,char* word1, int *common)
+{
+	char* word2 = (char *)calloc(SIZE, sizeof(char));
+	while (str2[pos2] != '\0')
+	{
+		word2 = getWord(str2, &pos2);
+		if (strcmp(word1, word2) == 0)
+		{
+			*(common) = *common + 1;
+			result = (char**)realloc(result, *common * sizeof(char *));
+			result[*common - 1] = word2;
+		}
+		pos2++;
+	}
+}
+
 char ** commonWords(char *str1, char *str2) {
 	int common = 0;
 	char** result = NULL;
 	int pos1 = 0, pos2 = 0;
 	char* word1 = (char *)calloc(SIZE, sizeof(char));
-	char* word2 = (char *)calloc(SIZE, sizeof(char));
 	if (str1 != NULL && str2 != NULL)
 	{
 		while (str1[pos1] != '\0')
 		{
 			word1 = getWord(str1, &pos1);
 			pos2 = 0;
-			while (str2[pos2] != '\0')
-			{
-				word2 = getWord(str2, &pos2);
-				if (strcmp(word1, word2) == 0)
-				{
-					common++;
-					result = (char**)realloc(result, common * sizeof(char *));
-					result[common - 1] = word2;
-				}
-				pos2++;
-			}
+			compareWordsWithString2(result, str2, pos2, word1, &common);
 			pos1++;
 		}
 	}
