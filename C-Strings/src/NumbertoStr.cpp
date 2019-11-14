18dd44b0d687e0389d5652cba4e9b0e4fc3a5f4d
manojdvs7 <52916630+manojdvs7@users.noreply.github.com>
2019-11-14 20:08:02 +0530

    Update NumbertoStr.cpp

--- C-Strings/src/NumbertoStr.cpp
+++ C-Strings/src/NumbertoStr.cpp
@@ -21,39 +21,62 @@ NOTES: Don't create new string.
 #include<stdlib.h>
 #include<math.h>
 
-int i = -1;
-void storeNumber(char* str, int num)
+int findNumberOfDigits(int n)
+{
+	int len = 0;
+	while (n)
+	{
+		len++;
+		n = n / 10;
+	}
+	return len;
+}
+
+void storeNumber(char* str, int num, int pos)
 {
 	if (num == 0)
 		return;
-	storeNumber(str, num / 10);
-	str[++i] = (num % 10)+'0';
+
+	storeNumber(str, num / 10, pos - 1);
+	str[pos] = (num % 10) + '0';
 }
 
-void storeDecimal(float decimal, char* str, int afterdecimal)
+int storeDecimal(float decimal, char* str, int afterdecimal, int pos)
 {
-	str[++i] = '.';
+	str[pos++] = '.';
 	int j = 1;
-	str[++i] = (decimal*pow(10, j))+'0';
+	str[pos++] = (decimal*pow(10, j)) + '0';
 	for (j = 2; j <= afterdecimal; j++)
 	{
 		int temp1 = decimal*pow(10, j);
 		int temp2 = (decimal*pow(10, j - 1));
 		temp1 = temp1 - (temp2 * 10);
-		str[++i] = temp1+'0';
+		str[pos++] = temp1 + '0';
 	}
+	return pos;
 }
 
 void number_to_str(float number, char *str, int afterdecimal) {
-	if (number < 0)
-	{
-		str[++i] = '-';
-		number = (-1) * number;
-	}
+
 	int integer = (int)number;
 	float decimal = number - integer;
-	storeNumber(str, integer);
+	int len = findNumberOfDigits(number);
+	int pos = len;
+	if (number < 0)
+	{
+		str[0] = '-';
+		integer = (-1) * integer;
+		decimal = (-1)*decimal;
+		pos++;
+	}
+	if (number > 0)
+		storeNumber(str, integer, --len);
+
+	if (number < 0)
+		storeNumber(str, integer, len);
+
 	if (afterdecimal > 0)
-		storeDecimal(decimal, str, afterdecimal);
-	str[++i] = '\0';
+		pos = storeDecimal(decimal, str, afterdecimal, len + 1);
+
+	str[pos] = '\0';
 }
